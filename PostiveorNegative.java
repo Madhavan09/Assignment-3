5d3b4d1d1de199af9fbb7f74dee53258accd6482
Madhavan09 <115532472+Madhavan09@users.noreply.github.com>
2022-10-12 23:22:18 +0530

    week1

---
+++ PostiveorNegative.java
@@ -0,0 +1,21 @@
+package week1.day3;
+
+public class PostiveorNegative {
+	public static void main(String[] args)   
+	{  
+		
+	//number to be check  
+	int num=9;  
+	//checks the number is greater than 0 or not  
+	if(num>0)  
+	{  
+	System.out.println("The number is positive");  
+	}  
+	//checks the number is less than 0 or not  
+	else if(num<0)  
+	{  
+	System.out.println("The number is negative");  
+}
+	}
+}
+}
---
+++ primeNumber.java
@@ -0,0 +1,28 @@
+package week1.day3;
+
+public class primeNumber {
+	public static void main(String[]args){
+		int n=14;
+		int count=0;
+		if(n<=1) {
+			System.out.println("The number is prime");
+			return;
+		}
+		for(int i=1;i<=n/2;i++) {
+		if(n%i==0) {
+			count++;
+		}
+		}
+		if(count>1) {
+			System.out.println("The number is not prime");
+		}
+		else {
+			System.out.println("The number is prime");
+		}
+	}
+		}
+		
+		
+	
+  
+
